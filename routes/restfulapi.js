815716f12d928bc27400605914dda0eb624f3d2d
mohamed salah hassan akel <sci.m.s.hassan@gmail.com>
2017-02-17 19:55:42 +0200

    test api

--- routes/restfulapi.js
+++ routes/restfulapi.js
@@ -1,1 +1,13 @@
+var assert = require('assert');
+
+var signupLoginOpt = require('../public/signup-loginCheck');
+
+app.post('/signup', function(req, res){
+    signupLoginOpt.signup(res.body.fname,res.body.lname,res.body.email,
+    res.body.password,res.body.country,res.body.city, function(rescallback){
+        app.get('/signup', function(req, res){
+            res.json(rescallback);
+        } );
+    });
+});
 
--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var express = require('express'),
         app = express();
 
 var port = process.env.PORT || 8080;
+app.use(express.static(__dirname + "/models"));
+app.use(express.static(__dirname + "/routes"));
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname));
-
-app.get('/', function(req, res){
-    res.send("hello world");
-});
+var restfulAPI = require('./routes');
 
 app.listen(port, function(){
     console.log("server running on %s", port);
