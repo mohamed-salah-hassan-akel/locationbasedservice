e39ab26ad011c587c9307b249066708a90e77d67
mohamed salah hassan akel <sci.m.s.hassan@gmail.com>
2017-02-17 17:06:33 +0200

    initial version

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 var express = require('express'),
-        fs = require('fs'),
+        http = require('http'),
         app = express();
-Object.assign = require('object-assign');
-var assert = require('assert');
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-        ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-        mongoURL = 'mongodb://salah:programming2015@ds139909.mlab.com:39909/locationappdb';
-var mongodb = require('mongodb');
 
-MongoClient.connect(mongoURL, function (err) {
-    assert.equal(null, err);
-    app.use('/', function (req, res) {
-        res.send({status: "connection established successfully"});
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+        ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+app.listen(port,ip, function (){
+    app.get('/', function(req, res){
+        res.send("hello world");
     });
 });
-
-
-app.listen(port, ip);
-console.log("server runnibg on http://%s:%S", ip, port);
-module.exports(app);
