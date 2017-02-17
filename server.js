9fea775ffcc279421d8ed1cf8d888dc6bead3c42
mohamed salah hassan akel <sci.m.s.hassan@gmail.com>
2017-02-17 16:41:32 +0200

    initial version

--- server.js
+++ server.js
@@ -1,75 +1,21 @@
 var express = require('express'),
         fs = require('fs'),
-        app = express(),
-        ejs = require('ejs'),
-        morgan = require('morgan');
+        app = express();
 Object.assign = require('object-assign');
-
+var assert = require('assert');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
         ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-        mongoURL = process.env.OPENSHIFT_MONGODB_URL || process.env.MONGO_URL,
-        mongoURLLabel = "";
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-            mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-            mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-            mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-            mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-    mongoUser = process.env[mongoServiceName + '_USER'];
+        mongoURL = 'mongodb://salah:programming2015@ds139909.mlab.com:39909/locationappdb';
+var mongodb = require('mongodb');
 
-    if (mongoHost && mongoPort && mongoDatabase) {
-        mongoURLLabel = mongoURL = 'mongodb://';
-        if (mongoUser && mongoPassword) {
-            mongoURL += mongoUser + ':' + mongoPassword + '@';
-        }
-        // Provide UI label that excludes user id and pw
-        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-        mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-
-    }
-}
-var db = null,
-        dbDetails = new Object();
-
-var initDb = function (callback) {
-    if (mongoURL == null)
-        return;
-
-    var mongodb = require('mongodb');
-    if (mongodb == null)
-        return;
-
-    mongodb.connect(mongoURL, function (err, conn) {
-        if (err) {
-            callback(err);
-            return;
-        }
-
-        db = conn;
-        dbDetails.databaseName = db.databaseName;
-        dbDetails.url = mongoURLLabel;
-        dbDetails.type = 'MongoDB';
-
-        console.log('Connected to MongoDB at: %s', mongoURL);
+MongoClient.connect(mongoURL, function (err) {
+    assert.equal(null, err);
+    app.use('/', function (req, res) {
+        res.send({status: "connection established successfully"});
     });
-};
-
-app.get('/', function (req, res) {
-// try to initialize the db on every request if it's not already
-// initialized.
-    if (!db) {
-        initDb(function (err) {});
-    }
 });
 
-// error handling
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something bad happened!');
-});
-initDb(function (err) {
-    console.log('Error connecting to Mongo. Message:\n' + err);
-});
+
 app.listen(port, ip);
 console.log("server runnibg on http://%s:%S", ip, port);
 module.exports(app);
